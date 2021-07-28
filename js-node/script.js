dd70bf1e911ae1170ac2e9d68024a5f5e69359b4
JEBesteman <86465468+JEBesteman@users.noreply.github.com>
2021-07-28 17:14:22 +0200

    Add files via upload

---
+++ js-node/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+
+// Dit is een grote som
+/* let multply = 1230941 * 1823794;
+console.log(multply);
+
+let division = 637263 / 54;
+console.log(division); */
